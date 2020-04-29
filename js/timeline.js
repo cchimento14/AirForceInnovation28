603f53afc58e15ac9413985bbd42127e4f39c5e1
FreeState-Dev <64382960+freestate-dev@users.noreply.github.com>
2020-04-29 12:34:53 -0700

    Update timeline.js

--- js/timeline.js
+++ js/timeline.js
@@ -1,35 +1,36 @@
-//Time Slider     
+// Time
 var dataTime = [
-  new Date(2018, 02),
-  new Date(2018, 05),
-  new Date(2018, 10),
-  new Date(2019, 02),
-  new Date(2019, 05),
-  new Date(2019, 10),
-  new Date(2020, 02)
+new Date(2018, 01),
+new Date(2018, 05),
+new Date(2018, 09),
+new Date(2019, 01),
+new Date(2019, 05),
+new Date(2019, 09),
+new Date(2020, 01)
 ];
 
+
 var sliderTime = d3
-  .sliderBottom()
-  .min(d3.min(dataTime))
-  .max(d3.max(dataTime))
-  .step(1000 * 60 * 60 * 24 * 90)
-  .width(300)
-  .tickFormat(d3.timeFormat('%Y'))
-  .tickValues(dataTime)
-  .default(new Date(2019, 02))
-  .on('onchange', val => {
-    d3.select('p#value-time').text(d3.timeFormat('%Y')(val));
-  });
+.sliderBottom()
+.min(d3.min(dataTime))
+.max(d3.max(dataTime))
+.step(1000*60*60*24*122)
+.width(800)
+.tickFormat(d3.timeFormat('%Y %b'))
+.tickValues(dataTime)
+.default(new Date(2018, 01))
+.on('onchange', val => {
+ d3.select('p#value-time').text(d3.timeFormat('%Y %b')(val));
+});
 
 var gTime = d3
-  .select('div#slider-time')
-  .append('svg')
-  .attr('width', 700)
-  .attr('height', 100)
-  .append('g')
-  .attr('transform', 'translate(30,30)');
+.select('div#slider-time')
+.append('svg')
+.attr('width', 900)
+.attr('height', 100)
+.append('g')
+.attr('transform', 'translate(30,30)');
 
 gTime.call(sliderTime);
+d3.select('p#value-time').text(d3.timeFormat('%Y %b')(sliderTime.value()));
 
-d3.select('p#value-time').text(d3.timeFormat('%Y')(sliderTime.value()));
