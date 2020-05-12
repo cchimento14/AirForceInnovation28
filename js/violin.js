56c1babaee50456bb38dcf7ab5ef84fe0f24b4c1
cchimento14 <cchimento14@jesuitmail.org>
2020-05-12 17:43:53 -0400

    move data read to violin.js

--- js/violin.js
+++ js/violin.js
@@ -1,1 +1,18 @@
-var dataset
+var dataset; //declare global varible, initially empty
+d3.csv("data.csv", function(d) {
+    return {
+        year: d.year,
+        solicitation: d.solicitation,
+        solicitation_date_gen: d.solicitation_date_gen,
+        employees: parseFloat(d.employees),
+        special: d.special,
+        pitch: d.pitch,
+        prev_award: d.prev_award,
+        phase: d.phase,
+        select: d.select,
+        financing_status: d.financing_status,
+        cost: parseFloat(d.cost)
+        };
+    }).then(function(data) {
+        dataset = data;
+    });
