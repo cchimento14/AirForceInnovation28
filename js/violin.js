14c2952e13cbf9ed6b5a4a6a6b46ff94edca8f53
FreeState-Dev <64382960+freestate-dev@users.noreply.github.com>
2020-05-12 16:21:24 -0700

    Update violin.js

--- js/violin.js
+++ js/violin.js
@@ -36,14 +36,14 @@ var svg = d3.select("#my_dataviz")
 
   // Build and Show the Y scale
   var y = d3.scaleLinear()
-    .domain([ 3.5,8 ])          // Note that here the Y scale is set manually
+    .domain([ 1,800 ])          // Note that here the Y scale is set manually
     .range([height, 0])
   svg.append("g").call( d3.axisLeft(y) )
 
   // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
   var x = d3.scaleBand()
     .range([ 0, width ])
-    .domain(["setosa", "versicolor", "virginica"])
+    .domain(["Select", "Non-select"])
     .padding(0.05)     // This is important: it is the space between 2 groups. 0 means no padding. 1 is the maximum.
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
