b24cd7f21d5e79fe309959cf3df0a73141218a66
cchimento14 <cchimento14@jesuitmail.org>
2020-05-13 14:46:20 -0400

    one at a time

--- js/violin.js
+++ js/violin.js
@@ -18,27 +18,27 @@ d3.csv("data.csv", function(d) {
     });
 
 // // set the dimensions and margins of the graph
-// var margin = {top: 10, right: 30, bottom: 30, left: 40},
-//     width = 460 - margin.left - margin.right,
-//     height = 400 - margin.top - margin.bottom;
-//
-// // append the svg object to the body of the page
-// var svg = d3.select("#my_dataviz")
-//   .append("svg")
-//     .attr("width", width + margin.left + margin.right)
-//     .attr("height", height + margin.top + margin.bottom)
-//   .append("g")
-//     .attr("transform",
-//           "translate(" + margin.left + "," + margin.top + ")");
+var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#my_dataviz")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
 //
 // // Read the data and compute summary statistics for each specie
 // //d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/iris.csv", function(data) { Davis. This is the part we can't use in v5
 //
 //   // Build and Show the Y scale
-//   var y = d3.scaleLinear()
-//     .domain([ 1,800 ])          // Note that here the Y scale is set manually
-//     .range([height, 0])
-//   svg.append("g").call( d3.axisLeft(y) )
+  var y = d3.scaleLinear()
+    .domain([ 0, d3.max(dataset,function(d){return d["employees"]; })])          // Note that here the Y scale is set manually
+    .range([height, 0])
+  svg.append("g").call( d3.axisLeft(y) )
 //
 //   // Build and Show the X scale. It is a band scale like for a boxplot: each group has an dedicated RANGE on the axis. This range has a length of x.bandwidth
 //   var x = d3.scaleBand()
